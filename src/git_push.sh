f44fce49aa37db09edf5e1a6f9ebcf179ad14f8a
humberto-aguiar <hbasf13@gmail.com>
2022-03-21 19:04:13 +0000

    update log from aws on date: $date

---
+++ src/git_push.sh
@@ -0,0 +1,16 @@
+# date
+dt=$(date '+%Y-%m-%d--%H:%M:%S')
+
+#path_papermill="/home/ubuntu/.pyenv/versions/3.8.0/envs/insiders_deploy/bin/papermill"
+#path_file="/home/ubuntu/project/Insiders-Loyalty-Program/src/notebooks/c15-deploy-4.ipynb"
+#path_log="/home/ubuntu/project/Insiders-Loyalty-Program/src/logs/c15-deploy-3-date:$dt.ipynb"
+
+# runing papermill deploy file on log path
+#$path_papermill $path_file $path_log
+
+# git push
+git add .
+
+git commit -m 'update log from aws on date: $date'
+
+git push origin main
