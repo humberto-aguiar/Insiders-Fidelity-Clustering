5df8210f3f909305bc3dab3a04054f5c8eae9200
humberto-aguiar <hbasf13@gmail.com>
2022-03-21 23:35:58 +0000

    add aws log from date: 2022-03-21--23:35:58

--- src/run_deploy_aws.sh
+++ src/run_deploy_aws.sh
@@ -5,8 +5,8 @@ dt=$(date '+%Y-%m-%d--%H:%M:%S')
 
 # paths
 path_papermill="/home/ubuntu/.pyenv/versions/3.8.0/envs/insiders_deploy/bin/papermill"
-path_file="/home/ubuntu/project/Insiders-Loyalty-Program/src/notebooks/c16-deploy-4.ipynb"
-path_log="/home/ubuntu/project/Insiders-Loyalty-Program/src/logs/c16-deploy-3-date:$dt.ipynb"
+path_file="/home/ubuntu/project/Insiders-Loyalty-Program/src/notebooks/c16-deploy-5.ipynb"
+path_log="/home/ubuntu/project/Insiders-Loyalty-Program/src/logs/c16-deploy-5-date:$dt.ipynb"
 
 # runing papermill deploy file on log path
 #$path_papermill $path_file $path_log
