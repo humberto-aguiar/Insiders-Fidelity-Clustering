db52a9de71d2e9a35fec06eed6d46a91a91c80fc
humberto-aguiar <hbasf13@gmail.com>
2022-03-23 09:31:01 -0300

    finish deploy on aws v1

--- src/run_deploy_aws.sh
+++ src/run_deploy_aws.sh
@@ -4,19 +4,20 @@
 dt=$(date '+%Y-%m-%d--%H:%M:%S')
 
 # paths
+#which papermill to use (venv)
 path_papermill="/home/ubuntu/.pyenv/versions/3.8.0/envs/insiders_deploy/bin/papermill"
-path_file="/home/ubuntu/project/Insiders-Loyalty-Program/src/notebooks/c15-deploy-4.ipynb"
-path_log="/home/ubuntu/project/Insiders-Loyalty-Program/src/logs/c15-deploy-4-date:$dt.ipynb"
+
+# lastest file to run
+file_to_execute="c17-deploy-v1.ipynb"
+
+# paths
+path_file="/home/ubuntu/project/Insiders-Loyalty-Program/src/notebooks/$file_to_execute"
+path_log="/home/ubuntu/project/Insiders-Loyalty-Program/src/logs/$file_to_execute-date:$dt.ipynb"
 
 # runing papermill deploy file on log path
 $path_papermill $path_file $path_log
 
-# date
-dt=$(date '+%Y-%m-%d--%H:%M:%S')
-
-#$path_papermill $path_file $path_log
-
-# git push
+# push to github
 git add .
 
 msg="add aws log from date: $dt"
